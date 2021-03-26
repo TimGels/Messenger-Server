21dc91862ca4797b2f9018b4cf319068f7f9f26d
TimGels <timgels1808@hotmail.com>
2021-03-26 14:48:12 +0100

    resolve merge conflicts again

---
+++ Client/Services/Converters/Base64ToImageConverter.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Messenger_Client.Services.Converters
+{
+    class Base64ToImageConverter
+    {
+    }
+}
